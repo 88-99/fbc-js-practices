73c75ba71345fb8258bbc9d305c7ce9a024f7ca3
88-99 <xz835.525o39@yahoo.com>
2023-07-21 21:43:57 +0900

    エラーなしを実装

---
+++ 05.asynchronous_sqlite3/async_await.js
@@ -0,0 +1,37 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(":memory:");
+
+function insertRecord() {
+  return new Promise(() => {
+    db.run("CREATE TABLE books (title TEXT)", () => {
+      const stmt = db.prepare("INSERT INTO books VALUES (?)");
+      for (let i = 0; i < 10; i++) {
+        stmt.run("title" + (i + 1));
+      }
+      stmt.finalize();
+    });
+  });
+}
+
+function selectRecord() {
+  return new Promise(() => {
+    setTimeout(function () {
+      db.each("SELECT rowid AS id, title FROM books", (err, row) => {
+        console.log(row.id + ": " + row.title);
+      });
+    }, 10);
+  });
+}
+
+function dropTable() {
+  db.run("DROP TABLE books");
+  db.close();
+}
+
+async function main() {
+  insertRecord();
+  await selectRecord();
+  dropTable();
+}
+
+main();
---
+++ 05.asynchronous_sqlite3/callback.js
@@ -0,0 +1,27 @@
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(":memory:");
+
+db.run("CREATE TABLE books (title TEXT)", () => {
+  const stmt = db.prepare("INSERT INTO books VALUES (?)");
+  for (let i = 0; i < 10; i++) {
+    stmt.run("title" + (i + 1));
+  }
+  stmt.finalize();
+
+  setTimeout(function () {
+    selectRecord();
+  }, 10);
+});
+
+function selectRecord() {
+  db.each(
+    "SELECT rowid AS id, title FROM books",
+    (err, row) => {
+      console.log(row.id + ": " + row.title);
+    },
+    () => {
+      db.run("DROP TABLE books");
+      db.close();
+    }
+  );
+}
