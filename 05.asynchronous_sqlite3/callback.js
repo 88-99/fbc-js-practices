11034412cdc2483a16166f374bb973bd9e4e5595
88-99 <xz835.525o39@yahoo.com>
2023-07-23 23:21:02 +0900

    lastID を使ってそれぞれの INSERT 文で自動採番された ID を出力するよう修正

--- 05.asynchronous_sqlite3/callback.js
+++ 05.asynchronous_sqlite3/callback.js
@@ -2,24 +2,31 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.run("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)", () => {
-  db.run("INSERT INTO books (title) VALUES ('title1')", () => {
-    db.run("INSERT INTO books (title) VALUES ('title2')", () => {
-      db.run("INSERT INTO books (title) VALUES ('title3')", () => {
-        db.run("INSERT INTO books (title) VALUES ('title4')", () => {
-          db.each(
-            "SELECT rowid AS id, title FROM books ORDER BY id ASC",
-            (err, row) => {
-              console.log(row.id + ": " + row.title);
-            },
-            () => {
-              db.run("DROP TABLE books", () => {
-                db.close();
-              });
-            }
-          );
-        });
+db.run("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)", function () {
+  db.run("INSERT INTO books (title) VALUES ('title1')", function () {
+    console.log(`lastID: ${this.lastID}`),
+      db.run("INSERT INTO books (title) VALUES ('title2')", function () {
+        console.log(`lastID: ${this.lastID}`),
+          db.run("INSERT INTO books (title) VALUES ('title3')", function () {
+            console.log(`lastID: ${this.lastID}`),
+              db.run(
+                "INSERT INTO books (title) VALUES ('title4')",
+                function () {
+                  console.log(`lastID: ${this.lastID}`),
+                    db.each(
+                      "SELECT id, title FROM books ORDER BY id ASC",
+                      (err, row) => {
+                        console.log(`${row.id}: ${row.title}`);
+                      },
+                      () => {
+                        db.run("DROP TABLE books", () => {
+                          db.close();
+                        });
+                      }
+                    );
+                }
+              );
+          });
       });
-    });
   });
 });
