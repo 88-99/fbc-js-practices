7d75fb914cba696a5b82612120a776c79b681d24
88-99 <xz835.525o39@yahoo.com>
2023-07-27 16:28:04 +0900

    2. コールバック形式の関数をラップして Promise 化し、then, catch の形式で使う を実装

--- 05.asynchronous_sqlite3/promise.js
+++ 05.asynchronous_sqlite3/promise.js
@@ -2,30 +2,57 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-function insertRecord() {
+function createTable() {
   return new Promise((resolve) => {
-    db.run("CREATE TABLE books (title TEXT)", () => {
-      const stmt = db.prepare("INSERT INTO books VALUES (?)");
-      for (let i = 0; i < 10; i++) {
-        stmt.run("title" + (i + 1));
-      }
-      stmt.finalize();
+    db.run("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)", () => {
       resolve();
     });
   });
 }
 
-insertRecord()
+createTable()
   .then(() => {
-    setTimeout(function () {
-      db.each("SELECT rowid AS id, title FROM books", (err, row) => {
-        console.log(row.id + ": " + row.title);
+    return new Promise((resolve) => {
+      db.run("INSERT INTO books (title) VALUES ('title1')", function () {
+        console.log(`lastID: ${this.lastID}`);
+        resolve();
       });
-    }, 10);
+    });
   })
   .then(() => {
-    () => {
-      db.run("DROP TABLE books");
-      db.close();
-    };
+    return new Promise((resolve) => {
+      db.run("INSERT INTO books (title) VALUES ('title2')", function () {
+        console.log(`lastID: ${this.lastID}`);
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    return new Promise((resolve) => {
+      db.run("INSERT INTO books (title) VALUES ('title3')", function () {
+        console.log(`lastID: ${this.lastID}`);
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    return new Promise((resolve) => {
+      db.run("INSERT INTO books (title) VALUES ('title4')", function () {
+        console.log(`lastID: ${this.lastID}`);
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    return db.each(
+      "SELECT id, title FROM books ORDER BY id ASC",
+      (err, row) => {
+        console.log(`${row.id}: ${row.title}`);
+      },
+      () => {
+        db.run("DROP TABLE books", () => {
+          db.close();
+        });
+      }
+    );
   });
