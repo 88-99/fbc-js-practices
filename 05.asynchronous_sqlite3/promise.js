8c0ffd6ed569b7d7d1d611282083f93cfb67b535
88-99 <xz835.525o39@yahoo.com>
2023-08-03 00:02:18 +0900

    関数に抽出

--- 05.asynchronous_sqlite3/promise.js
+++ 05.asynchronous_sqlite3/promise.js
@@ -11,55 +11,38 @@ function createTable() {
 }
 
 createTable()
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run("INSERT INTO books (title) VALUES ('title1')", function () {
-        console.log(`lastID: ${this.lastID}`);
-        resolve();
-      });
-    });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run("INSERT INTO books (title) VALUES ('title2')", function () {
-        console.log(`lastID: ${this.lastID}`);
-        resolve();
-      });
-    });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run("INSERT INTO books (title) VALUES ('title3')", function () {
-        console.log(`lastID: ${this.lastID}`);
-        resolve();
-      });
-    });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run("INSERT INTO books (title) VALUES ('title4')", function () {
-        console.log(`lastID: ${this.lastID}`);
-        resolve();
-      });
-    });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      return db.each(
-        "SELECT id, title FROM books ORDER BY id ASC",
-        (err, row) => {
-          console.log(`${row.id}: ${row.title}`);
-          resolve();
-        }
-      );
-    });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run("DROP TABLE books");
+  .then(() => insertTitle("title1"))
+  .then(() => insertTitle("title2"))
+  .then(() => insertTitle("title3"))
+  .then(() => insertTitle("title4"))
+  .then(() => selectRecord())
+  .then(() => doropTable())
+  .then(() => db.close());
+
+function insertTitle(title) {
+  return new Promise((resolve) => {
+    db.run("INSERT INTO books (title) VALUES (?)", [title], function () {
+      console.log(`lastID: ${this.lastID}`);
       resolve();
     });
-  })
-  .then(() => {
-    db.close();
   });
+}
+
+function selectRecord() {
+  return new Promise((resolve) => {
+    return db.each(
+      "SELECT id, title FROM books ORDER BY id ASC",
+      (err, row) => {
+        console.log(`${row.id}: ${row.title}`);
+        resolve();
+      }
+    );
+  });
+}
+
+function doropTable() {
+  return new Promise((resolve) => {
+    db.run("DROP TABLE books");
+    resolve();
+  });
+}
