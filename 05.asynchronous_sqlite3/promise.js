5b53cb07502a0cace73be66b664e524eeab03f4b
88-99 <xz835.525o39@yahoo.com>
2023-08-13 10:44:48 +0900

    then, catch の形式 を修正。each()も汎用的に使えるように。

--- 05.asynchronous_sqlite3/promise.js
+++ 05.asynchronous_sqlite3/promise.js
@@ -2,45 +2,53 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-function run(sql, title) {
+function run(sql, bindValue) {
   return new Promise((resolve) => {
-    db.run(sql, [title], function () {
+    db.run(sql, [bindValue], function () {
       resolve(this);
     });
   });
 }
 
-function each(sql) {
+function each(db, sql) {
   return new Promise((resolve) => {
-    return db.each(sql, (err, row) => console.log(`${row.id}: ${row.title}`));
+    const responses = [];
+    db.each(sql, (err, response) => {
+      responses.push(response);
+      resolve(responses);
+    });
   });
 }
 
-function close() {
-  db.close();
+function close(db) {
+  return new Promise((resolve) => {
+    db.close();
+    resolve();
+  });
 }
 
 run("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)")
-  .then(() =>
-    run("INSERT INTO books (title) VALUES (?)", "title1").then((response) => {
-      console.log(`lastID: ${response.lastID}`);
-    })
-  )
-  .then(() =>
-    run("INSERT INTO books (title) VALUES (?)", "title2").then((response) => {
-      console.log(`lastID: ${response.lastID}`);
-    })
-  )
-  .then(() =>
-    run("INSERT INTO books (title) VALUES (?)", "title3").then((response) => {
-      console.log(`lastID: ${response.lastID}`);
-    })
-  )
-  .then(() =>
-    run("INSERT INTO books (title) VALUES (?)", "title4").then((response) => {
-      console.log(`lastID: ${response.lastID}`);
-    })
-  )
-  .then(() => each("SELECT id, title FROM books ORDER BY id ASC"))
+  .then(() => run("INSERT INTO books (title) VALUES (?)", "title1"))
+  .then((record) => {
+    console.log(`lastID: ${record.lastID}`);
+  })
+  .then(() => run("INSERT INTO books (title) VALUES (?)", "title2"))
+  .then((record) => {
+    console.log(`lastID: ${record.lastID}`);
+  })
+  .then(() => run("INSERT INTO books (title) VALUES (?)", "title3"))
+  .then((record) => {
+    console.log(`lastID: ${record.lastID}`);
+  })
+  .then(() => run("INSERT INTO books (title) VALUES (?)", "title4"))
+  .then((record) => {
+    console.log(`lastID: ${record.lastID}`);
+  })
+  .then(() => each(db, "SELECT id, title FROM books ORDER BY id ASC"))
+  .then((records) => {
+    for (const record of records) {
+      console.log(`${record.id}: ${record.title}`);
+    }
+  })
   .then(() => run("DROP TABLE books"))
-  .then(() => close());
+  .then(() => close(db));
