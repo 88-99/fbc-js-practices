7c596b1f5c52164263e342412085b8abfa0faf0f
88-99 <xz835.525o39@yahoo.com>
2023-08-30 00:36:57 +0900

    コールバック形式で、NOT NULL 制約を追加して NULL を渡す。each() の complete callback をエラーハンドリング。

--- 05.asynchronous_sqlite3/callback_with_error.js
+++ 05.asynchronous_sqlite3/callback_with_error.js
@@ -2,48 +2,55 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-db.run("CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT)", function () {
-  db.run("INS INTO books (title) VALUES ('title1')", function (err) {
-    if (err) {
-      console.error("Error発生", err.message);
-    } else {
-      console.log(`lastID: ${this.lastID}`);
-    }
-    db.run("INSERT INTO books (title) VALUES ('title2')", function (err) {
+db.run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL)",
+  function () {
+    db.run("INSERT INTO books (title) VALUES (null)", function (err) {
       if (err) {
-        console.error("Error発生2", err.message);
+        console.error("Error発生1", err.message);
       } else {
         console.log(`lastID: ${this.lastID}`);
       }
-      db.run("INSERT INTO books (title) VALUES ('title3')", function (err) {
+      db.run("INSERT INTO books (title) VALUES (null)", function (err) {
         if (err) {
-          console.error("Error発生3", err.message);
+          console.error("Error発生2", err.message);
         } else {
           console.log(`lastID: ${this.lastID}`);
         }
-        db.run("INSERT INTO books (title) VALUES ('title4')", function (err) {
+        db.run("INSERT INTO books (title) VALUES (null)", function (err) {
           if (err) {
-            console.error("Error発生4", err.message);
+            console.error("Error発生3", err.message);
           } else {
             console.log(`lastID: ${this.lastID}`);
           }
-          db.each(
-            "SEL id, title FROM books ORDER BY id ASC",
-            (err, row) => {
-              if (err) {
-                console.error("Error発生5", err.message);
-              } else {
-                console.log(`${row.id}: ${row.title}`);
-              }
-            },
-            () => {
-              db.run("DROP TABLE books", () => {
-                db.close();
-              });
+          db.run("INSERT INTO books (title) VALUES (null)", function (err) {
+            if (err) {
+              console.error("Error発生4", err.message);
+            } else {
+              console.log(`lastID: ${this.lastID}`);
             }
-          );
+            db.each(
+              "SELECT id, name FROM books ORDER BY id ASC",
+              (err, row) => {
+                if (err) {
+                  console.error("Error発生5", err.message);
+                } else {
+                  console.log(`${row.id}: ${row.title}`);
+                }
+              },
+              (err) => {
+                if (err) {
+                  console.error("Error発生6", err.message);
+                } else {
+                  db.run("DROP TABLE books", () => {
+                    db.close();
+                  });
+                }
+              }
+            );
+          });
         });
       });
     });
-  });
-});
+  }
+);
