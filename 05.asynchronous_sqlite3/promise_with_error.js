5f7ea5911f48eec9de867aaa683488206fe4da44
88-99 <xz835.525o39@yahoo.com>
2023-09-01 00:29:45 +0900

    lastID を取得するためのコードを catch の上に移動

--- 05.asynchronous_sqlite3/promise_with_error.js
+++ 05.asynchronous_sqlite3/promise_with_error.js
@@ -15,45 +15,43 @@ run(
       "content1"
     )
   )
+  .then((record) => console.log(`lastID: ${record.lastID}`))
   .catch((err) => console.error("Error発生1", err.message))
-  .then((record) => {
-    console.log(`lastID: ${record.lastID}`);
-    return run(
+  .then(() =>
+    run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
       null,
       "content2"
-    );
-  })
+    )
+  )
+  .then((record) => console.log(`lastID: ${record.lastID}`))
   .catch((err) => console.error("Error発生2", err.message))
-  .then((record) => {
-    console.log(`lastID: ${record.lastID}`);
-    return run(
+  .then(() =>
+    run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
       null,
       "content3"
-    );
-  })
+    )
+  )
+  .then((record) => console.log(`lastID: ${record.lastID}`))
   .catch((err) => console.error("Error発生3", err.message))
-  .then((record) => {
-    console.log(`lastID: ${record.lastID}`);
-    return run(
+  .then(() =>
+    run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
       null,
       "content4"
-    );
-  })
+    )
+  )
+  .then((record) => console.log(`lastID: ${record.lastID}`))
   .catch((err) => console.error("Error発生4", err.message))
-  .then((record) => {
-    console.log(`lastID: ${record.lastID}`);
-    return each(
-      db,
-      "SELECT id, name, content FROM books ORDER BY id ASC",
-      (row) => console.log(`${row.id}: ${row.title}, ${row.content}`)
-    );
-  })
+  .then((record) =>
+    each(db, "SELECT id, name, content FROM books ORDER BY id ASC", (row) =>
+      console.log(`${row.id}: ${row.title}, ${row.content}`)
+    )
+  )
   .catch((err) => console.error("Error発生5", err.message))
   .then(() => run(db, "DROP TABLE books"))
   .then(() => close(db));
