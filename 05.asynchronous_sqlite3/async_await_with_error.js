cc4bc14381762a97cc1c51685bca892f0bd8a191
88-99 <xz835.525o39@yahoo.com>
2023-09-02 06:48:56 +0900

    各 INSERT, SELECT でエラーをキャッチできるよう修正

--- 05.asynchronous_sqlite3/async_await_with_error.js
+++ 05.asynchronous_sqlite3/async_await_with_error.js
@@ -18,7 +18,11 @@ async function main() {
       "content1"
     );
     console.log(`lastID: ${record1.lastID}`);
+  } catch (err) {
+    console.error("Error発生1", err.message);
+  }
 
+  try {
     const record2 = await run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
@@ -26,7 +30,11 @@ async function main() {
       "content2"
     );
     console.log(`lastID: ${record2.lastID}`);
+  } catch (err) {
+    console.error("Error発生2", err.message);
+  }
 
+  try {
     const record3 = await run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
@@ -34,7 +42,11 @@ async function main() {
       "content3"
     );
     console.log(`lastID: ${record3.lastID}`);
+  } catch (err) {
+    console.error("Error発生3", err.message);
+  }
 
+  try {
     const record4 = await run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
@@ -42,17 +54,25 @@ async function main() {
       "content4"
     );
     console.log(`lastID: ${record4.lastID}`);
+  } catch (err) {
+    console.error("Error発生4", err.message);
+  }
 
+  try {
     await each(
       db,
       "SELECT id, name, content FROM books ORDER BY id ASC",
       (row) => console.log(`${row.id}: ${row.title}, ${row.content}`)
     );
+  } catch (err) {
+    console.error("Error発生5", err.message);
+  }
 
+  try {
     await run(db, "DROP TABLE books");
     await close(db);
   } catch (err) {
-    console.error("Error発生", err.message);
+    console.error("Error発生6", err.message);
   }
 }
 
