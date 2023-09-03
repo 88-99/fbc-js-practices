2ab71b5ee88444a61598eb1c92bdee7d3d200f59
88-99 <xz835.525o39@yahoo.com>
2023-09-03 16:07:31 +0900

    async / await 形式で、Error オブジェクトのチェックと再スローを行うよう修正。

--- 05.asynchronous_sqlite3/async_await_with_error.js
+++ 05.asynchronous_sqlite3/async_await_with_error.js
@@ -5,12 +5,21 @@ import { run, each, close } from "./functions.js";
 
 async function main() {
   const db = new sqlite3.Database(":memory:");
+
   try {
     await run(
       db,
       "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL, content TEXT)"
     );
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error("Error発生0", err.message);
+    } else {
+      throw err;
+    }
+  }
 
+  try {
     const record1 = await run(
       db,
       "INSERT INTO books (title, content) VALUES (?, ?)",
@@ -19,7 +28,11 @@ async function main() {
     );
     console.log(`lastID: ${record1.lastID}`);
   } catch (err) {
-    console.error("Error発生1", err.message);
+    if (err instanceof Error) {
+      console.error("Error発生1", err.message);
+    } else {
+      throw err;
+    }
   }
 
   try {
@@ -31,7 +44,11 @@ async function main() {
     );
     console.log(`lastID: ${record2.lastID}`);
   } catch (err) {
-    console.error("Error発生2", err.message);
+    if (err instanceof Error) {
+      console.error("Error発生2", err.message);
+    } else {
+      throw err;
+    }
   }
 
   try {
@@ -43,7 +60,11 @@ async function main() {
     );
     console.log(`lastID: ${record3.lastID}`);
   } catch (err) {
-    console.error("Error発生3", err.message);
+    if (err instanceof Error) {
+      console.error("Error発生3", err.message);
+    } else {
+      throw err;
+    }
   }
 
   try {
@@ -55,7 +76,11 @@ async function main() {
     );
     console.log(`lastID: ${record4.lastID}`);
   } catch (err) {
-    console.error("Error発生4", err.message);
+    if (err instanceof Error) {
+      console.error("Error発生4", err.message);
+    } else {
+      throw err;
+    }
   }
 
   try {
@@ -65,14 +90,22 @@ async function main() {
       (row) => console.log(`${row.id}: ${row.title}, ${row.content}`)
     );
   } catch (err) {
-    console.error("Error発生5", err.message);
+    if (err instanceof Error) {
+      console.error("Error発生5", err.message);
+    } else {
+      throw err;
+    }
   }
 
   try {
     await run(db, "DROP TABLE books");
     await close(db);
   } catch (err) {
-    console.error("Error発生6", err.message);
+    if (err instanceof Error) {
+      console.error("Error発生6", err.message);
+    } else {
+      throw err;
+    }
   }
 }
 
